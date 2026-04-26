7276ce5edaa570e780bd8c8967281dd16c87850a
grapefruit <grapefruit021637@gmail.com>
2026-04-26 13:52:57 +0900

    Rename to 3D grapefruit, center header, and add thick colorful outlines with 3D shadows

--- update_style_final.bat
+++
@@ -1,4 +0,0 @@
-cd /d %~dp0
-"C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -m "Enlarge icon to 72px and rename professional section to Expertise"
-"C:\Program Files\Git\bin\git.exe" push
---
+++ update_ui_v3.bat
@@ -0,0 +1,4 @@
+cd /d %~dp0
+"C:\Program Files\Git\bin\git.exe" add .
+"C:\Program Files\Git\bin\git.exe" commit -m "Rename to 3D grapefruit, center header, and add thick colorful outlines with 3D shadows"
+"C:\Program Files\Git\bin\git.exe" push
