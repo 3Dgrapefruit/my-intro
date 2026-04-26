6a4c0f6788d36da2720df0caefc1020e49f76fcf
grapefruit <grapefruit021637@gmail.com>
2026-04-26 13:45:17 +0900

    Enlarge icon to 72px and rename professional section to Expertise

--- update_texts_final.bat
+++
@@ -1,4 +0,0 @@
-cd /d %~dp0
-"C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -m "Update section headings and texts for AI and Future Goals"
-"C:\Program Files\Git\bin\git.exe" push
