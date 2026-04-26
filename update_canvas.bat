78f336d308a87b1822f79fc1f6529bbd8654e512
grapefruit <grapefruit021637@gmail.com>
2026-04-26 12:41:20 +0900

    Update Strength text and enlarge 3D crane canvas to prevent clipping

---
+++ update_canvas.bat
@@ -0,0 +1,4 @@
+cd /d %~dp0
+"C:\Program Files\Git\bin\git.exe" add .
+"C:\Program Files\Git\bin\git.exe" commit -m "Update Strength text and enlarge 3D crane canvas to prevent clipping"
+"C:\Program Files\Git\bin\git.exe" push
--- update_final_design.bat
+++
@@ -1,4 +0,0 @@
-cd /d %~dp0
-"C:\Program Files\Git\bin\git.exe" add .
-"C:\Program Files\Git\bin\git.exe" commit -m "Refine 3D crane model, remove redundant text, and enhance UI contrast/depth"
-"C:\Program Files\Git\bin\git.exe" push
