5aaa6022b521493c4185880dc4eb0acd237b8cc2
3Dgrapefruit <grapefruit021637@gmail.com>
2026-05-05 13:39:12 +0900

    Add mock app link button to Future Goal section

---
+++ add_mock_link.bat
@@ -0,0 +1,4 @@
+cd /d %~dp0
+"C:\Program Files\Git\bin\git.exe" add .
+"C:\Program Files\Git\bin\git.exe" commit -m "Add mock app link button to Future Goal section"
+"C:\Program Files\Git\bin\git.exe" push
